9b91da6661bf4e7d13ff2910900456de2616d5b1
KhushiBhojwani <wrteam.jenil@gmail.com>
2025-07-01 11:42:59 +0530

    add column class

---
+++ src/app/column/page.tsx
@@ -0,0 +1,13 @@
+import React, { memo } from "react";
+export default function Column() {
+  return (
+    <div>
+      page
+      <div className="columns-3 ...">
+        <img className="aspect-3/2 ..." src="./../images /images(1).jpeg" />
+        {/* <img className="aspect-square ..." src="" />
+        <img className="aspect-square ..." src="" /> */}
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 import Image from "next/image";
+import Column from "./column/page";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="">
-      <h1 className="bg-indigo-900 text-yellow-100">Hello Next..</h1>
-      <button className="border-purple-200 text-purple-600  hover:bg-purple-600 hover:text-white active:bg-purple-700 ...">
+
+       <Link href="/column">Column</Link>
+      {/* <h1 className=" bg-indigo-600 text-green-900">Hello Next..</h1>
+      <button
+        className="border-purple-200 text-purple-600 border-2 border-black-900 border-dashed 
+                   bg-conic/decreasing   from-violet-700 via-lime-300 to-violet-700 h-16 w-40 flex items-center justify-center text-white hover:bg-purple-600  hover:text-white active:bg-purple-700 ..."
+      >
         Message
-      </button>
+      </button> */}
     </div>
   );
 }
